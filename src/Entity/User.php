3a4c6c75b9001c861ba61b47ce4c947873070f5d
aicha <aicha.samassa93@gmail.com>
2020-01-28 11:36:52 +0100

    premier git

---
+++ src/Entity/User.php
@@ -0,0 +1,68 @@
+<?php
+// src/Entity/User.php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $prenom;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
